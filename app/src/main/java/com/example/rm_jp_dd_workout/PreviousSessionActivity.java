86c75e0e1da8211f4a664c71c7fb07103e773ba9
Riddhiben Makwana <riddhimakwana77@gmail.com>
2024-02-17 10:32:04 -0500

    Midterm

---
+++ app/src/main/java/com/example/rm_jp_dd_workout/PreviousSessionActivity.java
@@ -0,0 +1,14 @@
+package com.example.rm_jp_dd_workout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreviousSessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous_session);
+    }
+}
